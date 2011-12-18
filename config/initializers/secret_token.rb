758460641d9f5c4cb49e32649dbd2bb6f24feea1
GroundskeeperWillie <wmacaulay@gmail.com>
2011-12-18 15:36:22 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Massage::Application.config.secret_token = 'c9862340226dacd849b43664923f2ed2ea5b44fe2aa63893c0f8c52ac015ae026b567b8848e665e2641ddc3b58b1cdecdb48bca1e4b4172913c9e3ff3b749bd9'
